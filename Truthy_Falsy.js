e675485c200c246dae0541d40d4ae545d468c642
ShvorShoshi <sanjidaalam0519@gmail.com>
2021-08-04 12:02:11 +0600

    add new file

---
+++ Truthy_Falsy.js
@@ -0,0 +1,26 @@
+//falsy => 0;
+      //=> "";
+      //=> false;
+      //=> undefined;
+      //=> Nan;
+      //=> null;
+
+var number = 1;
+
+if (number) {
+   console.log("condition is true"); 
+}
+else{
+    console.log("condition is false");
+}
+
+
+
+var name = "Shoshi";
+
+if (name) {
+   console.log("condition is true"); 
+}
+else{
+    console.log("condition is false");
+}
